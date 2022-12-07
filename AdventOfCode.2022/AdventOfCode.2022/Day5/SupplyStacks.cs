15b8e5afe2bbee41ba0a59d6a5e660bd72154de5
Hector Santana <hsantana@billtrust.com>
2022-12-07 14:31:04 -0500

    Added solution for Day 5 Part 1
    Added elapse time to check runtime

---
+++ AdventOfCode.2022/AdventOfCode.2022/Day5/SupplyStacks.cs
@@ -0,0 +1,110 @@
+﻿namespace AOC2022.Day5;
+
+public static class SupplyStacks
+{
+	private const string Day = "Day5";
+
+	public static void Part1()
+    {
+		var input = File.ReadAllLines($"{Day}/input.txt");
+
+		var stackIds = 
+			input.SkipWhile(l => !l.StartsWith(" 1")).First()
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+		var stacks = stackIds.ToDictionary(int.Parse, _ => new List<char>());
+
+		// Crates
+		foreach (var line in input)
+		{
+			if (line.StartsWith(" 1"))
+			{
+				break;
+			}
+
+			var stackId = 0;
+			for (var i = 0; i < line.Length; i+=4)
+			{
+				stackId++;
+				var crate = line.ElementAt(i + 1);
+
+				if (crate == ' ')
+				{
+					continue;
+				}
+
+				stacks[stackId].Add(crate);
+			}
+		}
+
+		// Moves
+		foreach (var line in input.SkipWhile(l => !l.StartsWith("move")))
+		{
+			var moves = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+			var quantity = int.Parse(moves[1]);
+			var fromStack = int.Parse(moves[3]);
+			var toStack = int.Parse(moves[5]);
+
+			for (var i = 1; i <= quantity; i++)
+			{
+				stacks[toStack].Insert(0, stacks[fromStack].First());
+				stacks[fromStack].RemoveAt(0);
+			}
+		}
+
+		var topCrates = stacks.Aggregate(string.Empty, (current, stack) => current + stack.Value.First());
+		Console.WriteLine($"Crates on Top: {topCrates}");
+	}
+
+    public static void Part2()
+    {
+	    var input = File.ReadAllLines($"{Day}/sample.txt");
+
+	    var stackIds =
+		    input.SkipWhile(l => !l.StartsWith(" 1")).First()
+			    .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+	    var stacks = stackIds.ToDictionary(int.Parse, _ => new List<char>());
+
+	    // Crates
+	    foreach (var line in input)
+	    {
+		    if (line.StartsWith(" 1"))
+		    {
+			    break;
+		    }
+
+		    var stackId = 0;
+		    for (var i = 0; i < line.Length; i += 4)
+		    {
+			    stackId++;
+			    var crate = line.ElementAt(i + 1);
+
+			    if (crate == ' ')
+			    {
+				    continue;
+			    }
+
+			    stacks[stackId].Add(crate);
+		    }
+	    }
+
+	    // Moves
+	    foreach (var line in input.SkipWhile(l => !l.StartsWith("move")))
+	    {
+		    var moves = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+		    var quantity = int.Parse(moves[1]);
+		    var fromStack = int.Parse(moves[3]);
+		    var toStack = int.Parse(moves[5]);
+
+		    for (var i = 1; i <= quantity; i++)
+		    {
+			    stacks[toStack].Insert(0, stacks[fromStack].First());
+			    stacks[fromStack].RemoveAt(0);
+		    }
+	    }
+
+	    var topCrates = stacks.Aggregate(string.Empty, (current, stack) => current + stack.Value.First());
+	    Console.WriteLine($"Crates on Top: {topCrates}");
+	}
+}
--- AdventOfCode.2022/AdventOfCode.2022/Program.cs
+++ AdventOfCode.2022/AdventOfCode.2022/Program.cs
@@ -1,7 +1,11 @@
-﻿using AOC2022.Day1;
+﻿using System.Diagnostics;
+using AOC2022.Day1;
 using AOC2022.Day2;
 using AOC2022.Day3;
 using AOC2022.Day4;
+using AOC2022.Day5;
+
+var timer = Stopwatch.StartNew();
 
 //CalorieCounting.Part1And2();
 
@@ -12,4 +16,10 @@ using AOC2022.Day4;
 //Rucksack.Part2();
 
 //CampCleanup.Part1();
-CampCleanup.Part2();
+//CampCleanup.Part2();
+
+SupplyStacks.Part1();
+
+timer.Stop();
+Console.WriteLine();
+Console.WriteLine($"Elapse Time: {timer.ElapsedMilliseconds}ms");
